e9be62c9480905a6205c5b8c0432c0ae97536c2f
heikki-john <joni.tauriainen@gmail.com>
2015-11-20 13:41:48 +0200

    update user

--- lotto/app/models/user.rb
+++ lotto/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+    validates :username, presence: true, uniqueness: true, length: { in: 4..20 }
+    validates :password, length: { in: 4..20 }
+
 end
--- lotto/db/schema.rb
+++ lotto/db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151120103849) do
+ActiveRecord::Schema.define(version: 20151120113000) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "username"
     t.string   "password"
     t.string   "email"
     t.datetime "created_at", null: false
